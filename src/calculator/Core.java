2a818c8069f74ed6c4a98dd06e60aaf3a33fbb0d
Z-MO7 <seu-email@exemplo.com>
2026-03-23 01:00:02 -0300

    feat: versão 1.0 finalizada com todas as operações e validações

--- src/calculator/Core.java
+++ src/calculator/Core.java
@@ -1,10 +1,69 @@
 package calculator;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Core {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		String operador;
+		double x, y, resultado = 0;
+		
+		System.out.println("[===== Calculadora do Z (v1.0) =====]");
+		System.out.println("Escolha uma das seguintes operações: + - / * ");
+		
+		operador = sc.next();
+		
+		switch(operador) {
+			case "+":
+				System.out.println("Você escolheu: Soma \nDigite a primeira parcela: ");
+				x = sc.nextDouble();
+				System.out.println("Digite a segunda parcela: ");
+				y = sc.nextDouble();
+				resultado = x + y;
+				System.out.printf("Resultado da soma: %.2f", resultado);
+				break;
+				
+			case "-":
+				System.out.println("Você escolheu: Subtração \nDigite o minuendo: ");
+				x = sc.nextDouble();
+				System.out.println("Digite o subtraendo: ");
+				y = sc.nextDouble();
+				resultado = x - y;
+				System.out.printf("Resultado da subtração: %.2f", resultado);
+				break;
+				
+			case "/":
+				System.out.println("Você escolheu: Divisão \nDigite numerador: ");
+				x = sc.nextDouble();
+				System.out.println("Digite o denominador: ");
+				y = sc.nextDouble();
+				if (y == 0) {
+					System.out.println("Impossível dividir por 0!");
+				} else {
+					resultado = x / y;
+					System.out.printf("Resultado da divisão: %.2f", resultado);
+				}
+				break;
+				
+			case "*":
+				System.out.println("Você escolheu: Multiplicação \nDigite o multiplicando: ");
+				x = sc.nextDouble();
+				System.out.println("Digite o multiplicador: ");
+				y = sc.nextDouble();
+				resultado = x * y;
+				System.out.printf("Resultado da multiplicação: %.2f", resultado);
+				break;	
+				
+			default: 
+				System.out.println("Erro: Operação não reconhecida.");
+				break;
+		} 
+		
+		sc.close();
 	}
 
 }
