5f0c38af0904e5bc453fc28b25b7cb4db2d70188
Z-MO7 <seu-email@exemplo.com>
2026-03-26 20:30:47 -0300

    feat: implementado laço de repetição e menu interativo (v1.1)

--- src/calculator/Core.java
+++ src/calculator/Core.java
@@ -9,59 +9,63 @@ public class Core {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		String operador;
+		String opcao;
 		double x, y, resultado = 0;
 		
 		System.out.println("[===== Calculadora do Z (v1.0) =====]");
-		System.out.println("Escolha uma das seguintes operações: + - / * ");
+		System.out.println("Escolha uma das seguintes opções: \n1 - Soma \n2 - Subtração \n3 - Multiplicação \n4 - Divisão \nQ - Sair");
+		opcao = sc.next();
 		
-		operador = sc.next();
-		
-		switch(operador) {
-			case "+":
-				System.out.println("Você escolheu: Soma \nDigite a primeira parcela: ");
-				x = sc.nextDouble();
-				System.out.println("Digite a segunda parcela: ");
-				y = sc.nextDouble();
-				resultado = x + y;
-				System.out.printf("Resultado da soma: %.2f", resultado);
-				break;
-				
-			case "-":
-				System.out.println("Você escolheu: Subtração \nDigite o minuendo: ");
-				x = sc.nextDouble();
-				System.out.println("Digite o subtraendo: ");
-				y = sc.nextDouble();
-				resultado = x - y;
-				System.out.printf("Resultado da subtração: %.2f", resultado);
-				break;
-				
-			case "/":
-				System.out.println("Você escolheu: Divisão \nDigite numerador: ");
-				x = sc.nextDouble();
-				System.out.println("Digite o denominador: ");
-				y = sc.nextDouble();
-				if (y == 0) {
-					System.out.println("Impossível dividir por 0!");
-				} else {
-					resultado = x / y;
-					System.out.printf("Resultado da divisão: %.2f", resultado);
-				}
-				break;
-				
-			case "*":
-				System.out.println("Você escolheu: Multiplicação \nDigite o multiplicando: ");
-				x = sc.nextDouble();
-				System.out.println("Digite o multiplicador: ");
-				y = sc.nextDouble();
-				resultado = x * y;
-				System.out.printf("Resultado da multiplicação: %.2f", resultado);
-				break;	
-				
-			default: 
-				System.out.println("Erro: Operação não reconhecida.");
-				break;
-		} 
+		while (!opcao.equalsIgnoreCase("q")) {		
+			switch(opcao) {
+				case "1":
+					System.out.println("Você escolheu: Soma \nDigite a primeira parcela: ");
+					x = sc.nextDouble();
+					System.out.println("Digite a segunda parcela: ");
+					y = sc.nextDouble();
+					resultado = x + y;
+					System.out.printf("Resultado da soma: %.2f\n", resultado);
+					break;
+					
+				case "2":
+					System.out.println("Você escolheu: Subtração \nDigite o minuendo: ");
+					x = sc.nextDouble();
+					System.out.println("Digite o subtraendo: ");
+					y = sc.nextDouble();
+					resultado = x - y;
+					System.out.printf("Resultado da subtração: %.2f\n", resultado);
+					break;
+					
+				case "3":
+					System.out.println("Você escolheu: Divisão \nDigite numerador: ");
+					x = sc.nextDouble();
+					System.out.println("Digite o denominador: ");
+					y = sc.nextDouble();
+					if (y == 0) {
+						System.out.println("Impossível dividir por 0!");
+					} else {
+						resultado = x / y;
+						System.out.printf("Resultado da divisão: %.2f\n", resultado);
+					}
+					break;
+					
+				case "4":
+					System.out.println("Você escolheu: Multiplicação \nDigite o multiplicando: ");
+					x = sc.nextDouble();
+					System.out.println("Digite o multiplicador: ");
+					y = sc.nextDouble();
+					resultado = x * y;
+					System.out.printf("Resultado da multiplicação: %.2f\n", resultado);
+					break;	
+					
+				default: 
+					System.out.println("Erro: Operação não reconhecida.\n");
+					break;
+			}
+			System.out.println("Escolha uma das seguintes opções: \n1 - Soma \n2 - Subtração \n3 - Multiplicação \n4 - Divisão \nQ - Sair");
+			opcao = sc.next();
+			
+		}
 		
 		sc.close();
 	}
